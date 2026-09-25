e2062f6b6930210f5a7b62217c1a66b4a2de0e77
Yamo <diahnet@naver.com>
2026-09-25 19:47:12 +0900

    feat: prepare syllable selection state

--- app/src/main/java/com/yamone/korean/ui/screens/SyllableScreen.kt
+++ app/src/main/java/com/yamone/korean/ui/screens/SyllableScreen.kt
@@ -13,6 +13,10 @@ import androidx.compose.material3.Card
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.saveable.rememberSaveable
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.yamone.korean.learning.BasicSyllableCatalog
