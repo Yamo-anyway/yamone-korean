907de63a072b0dfbb8d597d9ab5f1d0da0206b84
Yamo <diahnet@naver.com>
2026-09-25 21:42:58 +0900

    Expand first syllable practice set

--- app/src/main/java/com/yamone/korean/learning/BasicSyllableCatalog.kt
+++ app/src/main/java/com/yamone/korean/learning/BasicSyllableCatalog.kt
@@ -19,11 +19,24 @@ data class BasicSyllableLesson(
 object BasicSyllableCatalog {
     val lessons: List<BasicSyllableLesson> = listOf(
         BasicSyllableLesson("syllable_ga", "jamo_giyeok", "ㄱ", "jamo_a", "ㅏ", "가", "ga", SyllableVowelPlacement.RIGHT),
-        BasicSyllableLesson("syllable_na", "jamo_nieun", "ㄴ", "jamo_a", "ㅏ", "나", "na", SyllableVowelPlacement.RIGHT),
-        BasicSyllableLesson("syllable_mi", "jamo_mieum", "ㅁ", "jamo_i", "ㅣ", "미", "mi", SyllableVowelPlacement.RIGHT),
         BasicSyllableLesson("syllable_go", "jamo_giyeok", "ㄱ", "jamo_o", "ㅗ", "고", "go", SyllableVowelPlacement.BELOW),
+        BasicSyllableLesson("syllable_gu", "jamo_giyeok", "ㄱ", "jamo_u", "ㅜ", "구", "gu", SyllableVowelPlacement.BELOW),
+        BasicSyllableLesson("syllable_gi", "jamo_giyeok", "ㄱ", "jamo_i", "ㅣ", "기", "gi", SyllableVowelPlacement.RIGHT),
+
+        BasicSyllableLesson("syllable_na", "jamo_nieun", "ㄴ", "jamo_a", "ㅏ", "나", "na", SyllableVowelPlacement.RIGHT),
+        BasicSyllableLesson("syllable_no", "jamo_nieun", "ㄴ", "jamo_o", "ㅗ", "노", SyllableVowelPlacement.BELOW),
         BasicSyllableLesson("syllable_nu", "jamo_nieun", "ㄴ", "jamo_u", "ㅜ", "누", "nu", SyllableVowelPlacement.BELOW),
+        BasicSyllableLesson("syllable_ni", "jamo_nieun", "ㄴ", "jamo_i", "ㅣ", "니", "ni", SyllableVowelPlacement.RIGHT),
+
+        BasicSyllableLesson("syllable_ma", "jamo_mieum", "ㅁ", "jamo_a", "ㅏ", "마", "ma", SyllableVowelPlacement.RIGHT),
+        BasicSyllableLesson("syllable_mo", "jamo_mieum", "ㅁ", "jamo_o", "ㅗ", "모", "mo", SyllableVowelPlacement.BELOW),
+        BasicSyllableLesson("syllable_mu", "jamo_mieum", "ㅁ", "jamo_u", "ㅜ", "무", "mu", SyllableVowelPlacement.BELOW),
+        BasicSyllableLesson("syllable_mi", "jamo_mieum", "ㅁ", "jamo_i", "ㅣ", "미", "mi", SyllableVowelPlacement.RIGHT),
+
+        BasicSyllableLesson("syllable_ba", "jamo_bieup", "ㅂ", "jamo_a", "ㅏ", "바", "ba", SyllableVowelPlacement.RIGHT),
+        BasicSyllableLesson("syllable_bo", "jamo_bieup", "ㅂ", "jamo_o", "ㅗ", "보", "bo", SyllableVowelPlacement.BELOW),
         BasicSyllableLesson("syllable_bu", "jamo_bieup", "ㅂ", "jamo_u", "ㅜ", "부", "bu", SyllableVowelPlacement.BELOW),
+        BasicSyllableLesson("syllable_bi", "jamo_bieup", "ㅂ", "jamo_i", "ㅣ", "비", "bi", SyllableVowelPlacement.RIGHT),
     )
 
     fun byId(id: String): BasicSyllableLesson? = lessons.firstOrNull { it.id == id }
